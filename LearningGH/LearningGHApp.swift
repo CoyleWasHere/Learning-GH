e9997b622cda3710990ed843bd08db6a71131d5d
CoyleWasHere <64401148+CoyleWasHere@users.noreply.github.com>
2022-06-22 15:01:04 -0400

    Initial Commit

---
+++ LearningGH/LearningGHApp.swift
@@ -0,0 +1,17 @@
+//
+//  LearningGHApp.swift
+//  LearningGH
+//
+//  Created by Daniel Coyle on 6/22/22.
+//
+
+import SwiftUI
+
+@main
+struct LearningGHApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
